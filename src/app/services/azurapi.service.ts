64487cd7a3c8eb41e89b92f91614b723e3de98a2
Retropaint <darkglasses1122@gmail.com>
2022-11-28 17:03:07 +0800

    cleaned up azurapi

--- src/app/services/azurapi.service.ts
+++ src/app/services/azurapi.service.ts
@@ -19,7 +19,6 @@ export class AzurapiService {
     private storage: Storage, 
     private shortenedNames: ShortenedNamesService,
     private shipsService: ShipsService,
-    private httpClient: HttpClient
   ) {}
 
   async init() {
@@ -35,117 +34,91 @@ export class AzurapiService {
       .then(value => value.text())
       .then(ships => {
         JSON.parse(ships).forEach(async ship => {
-        
-          // only accept ships with a max level fleet tech bonus
-          //if(ship["fleetTech"]["statsBonus"]["collection"]) {
-          if(true) {
-  
-            let shipHasTech: boolean = false;
-  
-            if(ship["fleetTech"]["statsBonus"]["collection"]) {
-              shipHasTech = true;
-            }
 
+          // name corrections
+          if(ship["names"]["en"].includes("Pamiat")) {
+            ship["names"]["en"] = "Pamiat Merkuria";
+          } else if(ship["names"]["en"].includes("Royal.META")) {
+            ship["names"]["en"] = "Ark Royal META";
+          } else if(ship["names"]["en"].includes("Hiryu.META")) {
+            ship["names"]["en"] = "Hiryuu META";
+          }
+
+          // replace PR rarities with normal counterparts, and add dash for CSS
+          if(ship["rarity"] == "Super Rare" || ship["rarity"] == "Priority") {
+            ship["rarity"] = "Super-Rare";
+          }
+          if(ship["rarity"] == "Ultra Rare" || ship["rarity"] == "Decisive") {
+            ship["rarity"] = "Ultra-Rare";
+          }
+
+          if(ship["rarity"] == "Normal") {
+            ship["rarity"] = "Common";
+          }
+
+          let newShip: Ship = {
+            name: ship["names"]["en"],
+            id: ship["id"],
+            level: 1,
+            isObtained: false,
+            faction: this.shortenedNames.factions[ship["nationality"]],
+            rarity: ship["rarity"],
+            hull: this.shortenedNames.hulls[ship["hullType"]],
+            fallbackThumbnail: ship["thumbnail"],
+            hasRetrofit: ship["retrofit"],
+            retroHull: this.shortenedNames.hulls[ship['retrofitHullType']],
+          }
+
+          if(ship["fleetTech"]["statsBonus"]["collection"]) {
+            
             const fleetTech = ship["fleetTech"]["statsBonus"]["maxLevel"]
             const obtainFleetTech = ship["fleetTech"]["statsBonus"]["collection"];
-  
-            // there's a ' on Pamiat for some reason
-            if(ship["names"]["en"].includes("Pamiat")) {
-              ship["names"]["en"] = "Pamiat Merkuria";
-            } else if(ship["names"]["en"].includes("Royal.META")) {
-              ship["names"]["en"] = "Ark Royal META";
-            } else if(ship["names"]["en"].includes("Hiryu.META")) {
-              ship["names"]["en"] = "Hiryuu META";
+
+            // abbreviate applicable hulls
+            for(let i = 0; i < fleetTech['applicable'].length; i++) {
+              fleetTech['applicable'][i] = this.shortenedNames.applicableHulls[fleetTech['applicable'][i]]
             }
-  
-            // Normal LOL
-            if(ship["rarity"] == "Normal") {
-              ship["rarity"] = "Common";
+            for(let i = 0; i < obtainFleetTech['applicable'].length; i++) {
+              obtainFleetTech['applicable'][i] = this.shortenedNames.applicableHulls[obtainFleetTech['applicable'][i]]
             }
-  
-            // replace PR rarities with normal counterparts, and add dash for CSS
-            if(ship["rarity"] == "Super Rare" || ship["rarity"] == "Priority") {
-              ship["rarity"] = "Super-Rare";
-            }
-            if(ship["rarity"] == "Ultra Rare" || ship["rarity"] == "Decisive") {
-              ship["rarity"] = "Ultra-Rare";
-            }
-  
-            if(shipHasTech) {
-              // abbreviate applicable hulls
-              for(let i = 0; i < fleetTech['applicable'].length; i++) {
-                fleetTech['applicable'][i] = this.shortenedNames.applicableHulls[fleetTech['applicable'][i]]
-              }
-              for(let i = 0; i < obtainFleetTech['applicable'].length; i++) {
-                obtainFleetTech['applicable'][i] = this.shortenedNames.applicableHulls[obtainFleetTech['applicable'][i]]
+
+            newShip = {
+              ...newShip,
+              techBonus: parseInt(fleetTech["bonus"][1]),
+              techStat: this.shortenedNames.stats[fleetTech["stat"]],
+              appliedHulls: fleetTech["applicable"],
+              obtainStat: this.shortenedNames.stats[obtainFleetTech["stat"]],
+              obtainBonus: parseInt(obtainFleetTech["bonus"][1]),
+              obtainAppliedHulls: obtainFleetTech['applicable'],
+              techPoints: {
+                obtain: ship['fleetTech']['techPoints']['collection'],
+                maxLevel: ship['fleetTech']['techPoints']['maxLevel'],
+                maxLimitBreak: ship['fleetTech']['techPoints']['maxLimitBreak']
               }
             }
-  
-            let newShip: Ship;
-  
-            if(shipHasTech) {
-              newShip = {
-                name: ship["names"]["en"],
-                id: ship["id"],
-                level: 1,
-                isObtained: false,
-                faction: this.shortenedNames.factions[ship["nationality"]],
-                rarity: ship["rarity"],
-                hull: this.shortenedNames.hulls[ship["hullType"]],
-                fallbackThumbnail: ship["thumbnail"],
-                techBonus: parseInt(fleetTech["bonus"][1]),
-                techStat: this.shortenedNames.stats[fleetTech["stat"]],
-                appliedHulls: fleetTech["applicable"],
-                hasRetrofit: ship["retrofit"],
-                obtainStat: this.shortenedNames.stats[obtainFleetTech["stat"]],
-                obtainBonus: parseInt(obtainFleetTech["bonus"][1]),
-                retroHull: this.shortenedNames.hulls[ship['retrofitHullType']],
-                obtainAppliedHulls: obtainFleetTech['applicable'],
-                techPoints: {
-                  obtain: ship['fleetTech']['techPoints']['collection'],
-                  maxLevel: ship['fleetTech']['techPoints']['maxLevel'],
-                  maxLimitBreak: ship['fleetTech']['techPoints']['maxLimitBreak']
-                }
-              }
-            } else {
-              newShip = {
-                name: ship["names"]["en"],
-                id: ship["id"],
-                level: 1,
-                isObtained: false,
-                faction: this.shortenedNames.factions[ship["nationality"]],
-                rarity: ship["rarity"],
-                hull: this.shortenedNames.hulls[ship["hullType"]],
-                fallbackThumbnail: ship["thumbnail"],
-                hasRetrofit: ship["retrofit"],
-                retroHull: this.shortenedNames.hulls[ship['retrofitHullType']],
-              }
-            }
-  
-            // retain dynamic data of existing ships
-            if(savedShips != null && savedShips.length > 0) {
-              for(const savedShip of savedShips) {
-                if(savedShip.id == ship.id) {
-                  newShip.level = savedShip.level;
-                  newShip.isObtained = savedShip.isObtained;
-                  break;
-                }
-              }
-            }
-  
-            this.shipsService.ships.push(newShip);
-            /*
-            let name = encodeURI(newShip.name).replace(/%20/g, '_');
-            names.push(name);
-            ids.push(newShip.id);
-            
-            if(newShip.hasRetrofit) {
-              names.push(name + 'Kai')
-              const retroId = parseInt(newShip.id) + 3000
-              ids.push(retroId.toString());
-            }
-            */
           }
+
+          // retain dynamic data of existing ships
+          if(savedShips != null && savedShips.length > 0) {
+            const savedShip = savedShips.find(ship => ship.id == newShip.id);
+            if(savedShip) {
+              newShip.level = savedShip.level;
+              newShip.isObtained = savedShip.isObtained;
+            }
+          }
+
+          this.shipsService.ships.push(newShip);
+          /*
+          let name = encodeURI(newShip.name).replace(/%20/g, '_');
+          names.push(name);
+          ids.push(newShip.id);
+          
+          if(newShip.hasRetrofit) {
+            names.push(name + 'Kai')
+            const retroId = parseInt(newShip.id) + 3000
+            ids.push(retroId.toString());
+          }
+          */
         })
         this.hasLoaded = true;
         this.shipsService.save();
