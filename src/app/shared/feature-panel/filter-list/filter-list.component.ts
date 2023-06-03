57360bb489c53458bcde9ccb6b1c53d3bba68a3d
Retropaint <darkglasses1122@gmail.com>
2023-06-03 20:49:40 +0800

    removed Tempesta from filter list
    Royal Fortune is the only one there and she doesn't have tech anyway

--- src/app/shared/feature-panel/filter-list/filter-list.component.ts
+++ src/app/shared/feature-panel/filter-list/filter-list.component.ts
@@ -1,5 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { AppComponent } from 'src/app/app.component';
+import { Component, OnInit } from '@angular/core';
 import { FilterService } from 'src/app/services/filter.service';
 import { MiscService } from 'src/app/services/misc.service';
 import { SettingsDataService } from 'src/app/services/settings-data.service';
@@ -13,7 +12,7 @@ export class FilterListComponent implements OnInit {
 
   hullNames: string[] = ["DD", "CL", "CA", "CV", "BB", "SS", "Others", "All"]
   statNames: string[] = ["FP", "TRP", "AVI", "AA", "RLD", "HIT", "ASW", "EVA", "HP", "No Tech", "All"];
-  factionNames: string[] = ["USS", "HMS", "IJN", "KMS", "ROC", "RN", "SN", "FFNF", "MNF", "MOT", "META", "All"];
+  factionNames: string[] = ["USS", "HMS", "IJN", "KMS", "ROC", "RN", "SN", "FFNF", "MNF", "META", "All"];
   rarityNames: string[] = ["Common", "Rare", "Elite", "Super-Rare", "Ultra-Rare", "All"];
   statusNames: string[] = ["W.I.P", "Unobtained", "Maxed", "All"];
 
