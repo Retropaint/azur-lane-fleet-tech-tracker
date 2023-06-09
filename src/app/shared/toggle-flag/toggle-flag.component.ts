7eed07c936648ecbe623af85199f6c1579be9b0c
Retropaint <darkglasses1122@gmail.com>
2023-06-09 15:35:38 +0800

    fixed toggle flag diamond for ship level editor

--- src/app/shared/toggle-flag/toggle-flag.component.ts
+++ src/app/shared/toggle-flag/toggle-flag.component.ts
@@ -8,6 +8,7 @@ import { Component, Input, OnInit } from '@angular/core';
 export class ToggleFlagComponent implements OnInit {
 
   @Input() toggle: boolean = false;
+  @Input() toggleContainer: boolean = false;
 
   constructor() { }
 
